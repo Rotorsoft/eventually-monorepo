a81c4b5dc9773d1a4ed58929c4443b9524386a28
Mohamed Abdelwahab <mohamed.abdelwahab@andela.com>
2021-11-11 13:03:45 +0200

    update accounts service with 3.1 changes

--- services/accounts/src/index.ts
+++ services/accounts/src/index.ts
@@ -1,13 +1,28 @@
 import { app } from "@rotorsoft/eventually";
 import { ExpressApp } from "@rotorsoft/eventually-express";
-import * as commands from "./accounts.commands";
-import * as events from "./accounts.events";
+import { Commands } from "./accounts.commands";
+import { Events } from "./accounts.events";
 import * as policies from "./accounts.policies";
 import * as systems from "./accounts.systems";
+import * as schemas from "./accounts.schemas";
+
 
 app(new ExpressApp())
-  .withCommands(commands.factory)
-  .withEvents(events.factory)
+  .withSchemas<Commands>({
+    CreateAccount1: schemas.CreateAccount1,
+    CreateAccount2: schemas.CreateAccount2,
+    CreateAccount3: schemas.CreateAccount3,
+    CompleteIntegration: schemas.CompleteIntegration
+  })
+  .withSchemas<Events>({
+    AccountCreated: schemas.AccountCreated,
+    Account1Created: schemas.Account1Created,
+    Account2Created: schemas.Account2Created,
+    Account3Created: schemas.Account3Created,
+    IntegrationCompleted: schemas.IntegrationCompleted
+  })
+  .withPrivate<Commands>("CreateAccount1", "CreateAccount2", "CreateAccount3", "CompleteIntegration")
+  .withPrivate<Events>("Account1Created", "Account2Created", "Account3Created")
   .withEventHandlers(
     policies.IntegrateAccount1,
     policies.IntegrateAccount2,
