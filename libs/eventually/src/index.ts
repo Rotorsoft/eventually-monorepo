08949c9e1644f56113d326093d45fef5a29142bf
Roger Torres <roger.torres@andela.com>
2021-11-24 11:13:50 -0500

    export singleton

--- libs/eventually/src/index.ts
+++ libs/eventually/src/index.ts
@@ -3,12 +3,13 @@ import { singleton } from "./singleton";
 import { InMemoryApp } from "./__dev__";
 
 export * from "./app";
+export * from "./builder";
 export * from "./config";
 export * from "./interfaces";
 export * from "./log";
+export * from "./singleton";
 export * from "./types";
 export * from "./utils";
-export * from "./builder";
 
 export const app = singleton(function app(app?: AppBase) {
   return app || new InMemoryApp();
