a0cd0364073e7fbc56e92047985c730847953a66
Roger Torres <roger.torres@andela.com>
2022-05-30 16:21:57 -0400

    fix(eventually-broker): remove SsePushChannel naive implementation

--- libs/eventually-broker/src/channels/index.ts
+++ libs/eventually-broker/src/channels/index.ts
@@ -1,5 +1,4 @@
 export * from "./PostgresPullChannel";
 export * from "./HttpPostPushChannel";
-export * from "./SsePushChannel";
 export * from "./VoidPullChannel";
 export * from "./VoidPushChannel";
