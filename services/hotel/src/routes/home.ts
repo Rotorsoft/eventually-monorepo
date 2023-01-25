76c712766b10e26958fb12e0ede3fa7f7d7bb747
Roger Torres <roger.torres@andela.com>
2023-01-25 15:45:19 -0500

    chore: fix hotel ui

--- services/hotel/src/routes/home.ts
+++ services/hotel/src/routes/home.ts
@@ -5,27 +5,32 @@ import { Next30Days } from "../Next30Days.projector";
 import { addDays } from "../utils";
 
 export const home = async (_: Request, res: Response): Promise<void> => {
-  const rooms = await client().read(Hotel, [
-    "Room-101",
-    "Room-102",
-    "Room-103",
-    "Room-104",
-    "Room-105"
-  ]);
-  const tomorrow = addDays(new Date(), 1).toISOString().substring(0, 10);
-  const days = await client().read(Next30Days, [tomorrow]);
-  res.render("home", {
-    tomorrow: days[tomorrow]?.state,
-    rooms: Object.values(rooms)
+  try {
+    const _rooms = await client().read(Hotel, [
+      "Room-101",
+      "Room-102",
+      "Room-103",
+      "Room-104",
+      "Room-105"
+    ]);
+    const tomorrow_key = addDays(new Date(), 1).toISOString().substring(0, 10);
+    const days = await client().read(Next30Days, [tomorrow_key]);
+    const tomorrow = days[tomorrow_key]?.state;
+    const rooms = Object.values(_rooms)
       .map((r) => r.state)
       .map(({ number, type, price, reserved }) => ({
         number,
         type,
         price,
-        reserved: Object.entries(reserved || {})
-          .map(([day, id]) => ({ day, id }))
-          .sort()
+        reserved: reserved ? Object.keys(reserved).length : 0
       }))
-      .sort((a, b) => a.number - b.number)
-  });
+      .sort((a, b) => a.number - b.number);
+
+    res.render("home", {
+      tomorrow,
+      rooms
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
