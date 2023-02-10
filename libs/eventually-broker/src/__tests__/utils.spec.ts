3d7943b38bfe2daf9608e580400d2d3242dd687c
Roger Torres <roger.torres@andela.com>
2023-02-10 15:25:55 -0500

    fix: test

--- libs/eventually-broker/src/__tests__/utils.spec.ts
+++ libs/eventually-broker/src/__tests__/utils.spec.ts
@@ -3,7 +3,7 @@ import { toQueryString, toAxiosRequestHeaders } from "../utils";
 describe("utils", () => {
   it("should work", () => {
     const qs = toQueryString({ a: 1, b: 2 });
-    expect(qs).toBe("&a=1&b=2");
+    expect(qs).toBe("a=1&b=2");
 
     const obj = { h1: true, h2: "val2", h3: 3 };
     const rh = toAxiosRequestHeaders(obj);
